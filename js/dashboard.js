75fd3a35aa16c4ad3e3cf658eaf36bc551b4c4ff
djsbalakrishnan <pumba.the.dev@gmail.com>
2018-10-28 20:37:48 +0530

    First version of website

---
+++ js/dashboard.js
@@ -0,0 +1,132 @@
+$(document).ready(function() {
+    function loadFunction(){
+        window.loading_screen.finish();
+    }
+    window.loading_screen = window.pleaseWait({
+        logo: "images/loading.gif",
+        backgroundColor: '#0085c3',
+    });
+    setTimeout(loadFunction, 3000);
+
+    google.setOnLoadCallback(function(){
+        drawVisualization(state_2018, 2018);
+    });
+
+    $('select').material_select();
+
+    $("#year-selected").change(function(){
+        var year = $("#year-selected").val();
+        console.log(year);
+        if(year === "2016"){
+            window.loading_screen = window.pleaseWait({
+                logo: "images/loading.gif",
+                backgroundColor: '#0085c3',
+            });
+            setTimeout(loadFunction, 1500);
+            drawVisualization(state_2016, int(year));
+        }
+        else if(year === "2017"){
+            window.loading_screen = window.pleaseWait({
+                logo: "images/loading.gif",
+                backgroundColor: '#0085c3',
+            });
+            setTimeout(loadFunction, 1500);
+            drawVisualization(state_2017, int(year));
+        }
+        else if(year==="2018"){
+            window.loading_screen = window.pleaseWait({
+                logo: "images/loading.gif",
+                backgroundColor: '#0085c3',
+            });
+            setTimeout(loadFunction, 1500);
+            drawVisualization(state_2018, int(year));
+        }
+    });
+    
+    function findStateDetails(rowID, year){
+        $('#visualization').animate({left: '-200px'}, {duration: 2500});
+        rowID += 1;
+        var state_object = null;
+        var state_year_list = state_dis_object[year];
+        
+        var state_year_object = null;
+        
+        for(var i=0; i<state_year_list.length; i++){
+            if(state_year_list[i].id == rowID){
+                state_year_object = state_year_list[i];
+            }
+        }
+
+        for(var i = 0; i < state_list.length; i++){
+            if(state_list[i].id === rowID){
+                state_object = state_list[i];
+                console.log(state_object);
+            }
+        }
+        if (state_object != null){
+            window.loading_screen = window.pleaseWait({
+                logo: "images/loading.gif",
+                backgroundColor: '#0085c3',
+            });
+            setTimeout(loadFunction, 2000);
+            var image_string = "images/" + year + "/" + state_object.id + ".png"; 
+            var image_tag = '<img src="' + image_string + '" class="responsive-img">';
+            $("#visualization").html(image_tag);
+            var sec_column_html = '<ul class="collapsible expandable" style="margin-top:100px text-align:left"><li class="active" style="text-align:left"> <div class="collapsible-header"><i class="material-icons"> place</i>' +  state_object.state_name + '</div><div class="collapsible-body"><span style="text-align:left;"><B>Population: </B><br>' + state_object.population + '<br><br><B>Literacy Rate: </B><br>' + state_object.literacy_rate +  '<br><br><B>State GDP: </B><br>' + state_object.state_gdp + '<br><br><B>Household Work Demanded: </B><br>' + state_year_object.state_household_work_demanded +  '<br><br><B>Person Demanded Work: </B><br>' + state_year_object.state_person_demanded_work + '</span></div></li><li style="text-align:left"><div class="collapsible-header"><i class="material-icons">arrow_drop_down</i>More</div><div class="collapsible-body"><span style="text-align:left;"> <B>State Distress Value</B><br>' + state_year_object.state_dis + '<br><br><B>Sex Ratio: </B><br>' + state_object.sex_ration +  '<br><br><B>Capital: </B><br>' + state_object.capital + '</span></div></li></ul>'
+            console.log(sec_column_html);
+            $("#sec-column").html(sec_column_html);
+            $('.collapsible').collapsible('open', 0);
+        }
+    }
+
+    function drawVisualization(rows, year) {
+        console.log("drawVisualization Called");
+        var dataTable = new google.visualization.DataTable();
+        dataTable.addColumn('string', 'State Code');
+        dataTable.addColumn('string', 'State');
+        dataTable.addColumn('number', 'Distress');
+        dataTable.addColumn({type: 'string', role: 'tooltip'});
+
+        dataTable.addRows(rows);
+
+        var opts = {
+            region: 'IN',
+            domain:'IN',
+            displayMode: 'regions',
+            colorAxis: {
+                values:[0, 1.2, 1.21, 2.5, 2.5, 3.5],
+                colors:['#29a04d', '29a04d', '#ffb453', '#ffb453', '#cc3232', '#cc3232']
+            },
+            resolution: 'provinces',
+            defaultColor: '#979797',
+            width: 640,
+            height: 480
+        };
+
+        var geochart = new google.visualization.GeoChart(
+            document.getElementById('visualization')
+        );
+
+        function myClickHandler(){
+            var selection = geochart.getSelection();
+            var message = '';
+            for (var i = 0; i < selection.length; i++) {
+                var item = selection[i];
+                if (item.row != null && item.column != null) {
+                    message += '{row:' + item.row + ',column:' + item.column + '}';
+                } else if (item.row != null) {
+                    row = item.row;
+                } else if (item.column != null) {
+                    message += '{column:' + item.column + '}';
+                }
+            }
+            findStateDetails(row, year);
+        }
+
+        google.visualization.events.addListener(geochart, 'select', myClickHandler);
+
+        geochart.draw(dataTable, opts);
+    };
+});
+
+
---
+++ js/state_year_details.js
@@ -0,0 +1,476 @@
+state_dis_object = {  
+    "2017": [
+        { 
+        "id":1,
+        "state_dis":0.99,
+        "state_household_work_demanded": 172778.4844,
+        "state_person_demanded_work": 198110.1406
+        },
+        {  
+        "id":2,
+        "state_dis":1.42,
+        "state_household_work_demanded": 347487.375,
+        "state_person_demanded_work": 609095.0625 
+        },
+        {  
+        "id":3,
+        "state_dis":0.58,
+        "state_household_work_demanded": 8625.682617,
+        "state_person_demanded_work": 9277.317383
+        },
+        {  
+        "id":4,
+        "state_dis":1.21,
+        "state_household_work_demanded": 68222.29688,
+        "state_person_demanded_work": 108631.75
+        },
+        {  
+        "id":5,
+        "state_dis":0.9,
+        "state_household_work_demanded": 76119.78125,
+        "state_person_demanded_work": 94642.3125
+        },
+        {  
+        "id":6,
+        "state_dis":1.07,
+        "state_household_work_demanded": 107963.375,
+        "state_person_demanded_work": 215242.2969
+        },
+        {  
+        "id":7,
+        "state_dis":0.14,
+        "state_household_work_demanded": 3428.600098,
+        "state_person_demanded_work": 3522.600098
+        },
+        {  
+        "id":8,
+        "state_dis":0.9,
+        "state_household_work_demanded": 34983.52344,
+        "state_person_demanded_work": 57717.22266
+        },
+        {  
+        "id":9,
+        "state_dis":0.48,
+        "state_household_work_demanded": 17501.06641,
+        "state_person_demanded_work": 26977.68359
+        },
+        {  
+        "id":10,
+        "state_dis":1.02,
+        "state_household_work_demanded": 56449.22266,
+        "state_person_demanded_work": 75170.64844
+        },
+        {  
+        "id":11,
+        "state_dis":0.96,
+        "state_household_work_demanded": 35730.05859,
+        "state_person_demanded_work": 57029.71875
+        },
+        {  
+        "id":12,
+        "state_dis":0.89,
+        "state_household_work_demanded": 85459.64063,
+        "state_person_demanded_work": 122346.4531
+        },
+        {  
+        "id":13,
+        "state_dis":0.81,
+        "state_household_work_demanded": 67012.15625,
+        "state_person_demanded_work": 147601.1563
+        },
+        {  
+        "id":14,
+        "state_dis":1.15,
+        "state_household_work_demanded": 104219.9453,
+        "state_person_demanded_work": 121227.7031
+        },
+        {  
+        "id":15,
+        "state_dis":0.96,
+        "state_household_work_demanded": 80432.10156,
+        "state_person_demanded_work": 152457.9375
+        },
+        {  
+        "id":16,
+        "state_dis":0.96,
+        "state_household_work_demanded": 54836.64063,
+        "state_person_demanded_work": 103051.3359
+        },
+        {  
+        "id":17,
+        "state_dis":0.95,
+        "state_household_work_demanded": 56632.11719,
+        "state_person_demanded_work": 61441.53516
+        },
+        {  
+        "id":18,
+        "state_dis":0.99,
+        "state_household_work_demanded": 45491.04297,
+        "state_person_demanded_work": 66980.74219
+        },
+        {  
+        "id":19,
+        "state_dis":0.75,
+        "state_household_work_demanded": 23228.1875,
+        "state_person_demanded_work": 24194.625
+        },
+        {  
+        "id":20,
+        "state_dis":0.85,
+        "state_household_work_demanded": 95363.27344,
+        "state_person_demanded_work": 162772.6719
+        },
+        {  
+        "id":21,
+        "state_dis":1.07,
+        "state_household_work_demanded": 38869,
+        "state_person_demanded_work": 47601.35547
+        },
+        {  
+        "id":22,
+        "state_dis":1.32,
+        "state_household_work_demanded": 157885.5938,
+        "state_person_demanded_work": 245627.8906
+        },
+        {  
+        "id":23,
+        "state_dis":1.0,
+        "state_household_work_demanded": 70862.89844,
+        "state_person_demanded_work": 100169.8984
+        },
+        {  
+        "id":24,
+        "state_dis":1.0,
+        "state_household_work_demanded": 80431.51563,
+        "state_person_demanded_work": 102089.3828
+        },
+        {  
+        "id":25,
+        "state_dis":1.17,
+        "state_household_work_demanded": 37133.03906,
+        "state_person_demanded_work": 50724.46094
+        },
+        {  
+        "id":26,
+        "state_dis":2.47,
+        "state_household_work_demanded": 329378.9375,
+        "state_person_demanded_work": 542752.375
+        }
+    ],
+    "2016": [
+        { 
+        "id":1,
+        "state_dis":1.01,
+        "state_household_work_demanded": 183434.31,
+        "state_person_demanded_work": 216926.44
+        },
+        {  
+        "id":2,
+        "state_dis":1.44,
+        "state_household_work_demanded": 319018.19,
+        "state_person_demanded_work": 564164.31
+        },
+        {  
+        "id":3,
+        "state_dis":0.42,
+        "state_household_work_demanded": 11469.6,
+        "state_person_demanded_work": 12659.13
+        },
+        {  
+        "id":4,
+        "state_dis":1.35,
+        "state_household_work_demanded": 70758.8,
+        "state_person_demanded_work": 106544.04
+        },
+        {  
+        "id":5,
+        "state_dis":0.92,
+        "state_household_work_demanded": 77052.64,
+        "state_person_demanded_work": 97571.09
+        },
+        {  
+        "id":6,
+        "state_dis":1.1,
+        "state_household_work_demanded": 97818.02,
+        "state_person_demanded_work": 198996.17
+        },
+        {  
+        "id":7,
+        "state_dis":0.08,
+        "state_household_work_demanded": 3486,
+        "state_person_demanded_work": 3587.2
+        },
+        {  
+        "id":8,
+        "state_dis":0.81,
+        "state_household_work_demanded": 28965.39,
+        "state_person_demanded_work": 50929.15
+        },
+        {  
+        "id":9,
+        "state_dis":0.59,
+        "state_household_work_demanded": 18081.33,
+        "state_person_demanded_work": 28757.5
+        },
+        {  
+        "id":10,
+        "state_dis":1.26,
+        "state_household_work_demanded": 58335.78,
+        "state_person_demanded_work": 77655.54
+        },
+        {  
+        "id":11,
+        "state_dis":1.03,
+        "state_household_work_demanded": 31403.09,
+        "state_person_demanded_work": 48239.47
+        },
+        {  
+        "id":12,
+        "state_dis":1.01,
+        "state_household_work_demanded": 94769.13,
+        "state_person_demanded_work": 143425.91
+        },
+        {  
+        "id":13,
+        "state_dis":1.04,
+        "state_household_work_demanded": 68137.05,
+        "state_person_demanded_work": 171633.89
+        },
+        {  
+        "id":14,
+        "state_dis":1.12,
+        "state_household_work_demanded": 113626.53,
+        "state_person_demanded_work": 133034.06
+        },
+        {  
+        "id":15,
+        "state_dis":0.98,
+        "state_household_work_demanded": 67500.03,
+        "state_person_demanded_work": 134085.44
+        },
+        {  
+        "id":16,
+        "state_dis":1.02,
+        "state_household_work_demanded": 46668.36,
+        "state_person_demanded_work": 90591.76
+        },
+        {  
+        "id":17,
+        "state_dis":0.95,
+        "state_household_work_demanded": 58420,
+        "state_person_demanded_work": 62100.09
+        },
+        {  
+        "id":18,
+        "state_dis":0.87,
+        "state_household_work_demanded": 43263.04,
+        "state_person_demanded_work": 62137.3
+        },
+        {  
+        "id":19,
+        "state_dis":0.76,
+        "state_household_work_demanded": 22944.25,
+        "state_person_demanded_work": 24968.44
+        },
+        {  
+        "id":20,
+        "state_dis":0.87,
+        "state_household_work_demanded": 88631.3,
+        "state_person_demanded_work": 151385.14
+        },
+        {  
+        "id":21,
+        "state_dis":1.07,
+        "state_household_work_demanded": 30133.75,
+        "state_person_demanded_work": 36706.93
+        },
+        {  
+        "id":22,
+        "state_dis":1.38,
+        "state_household_work_demanded": 159238.69,
+        "state_person_demanded_work": 245185.2
+        },
+        {  
+        "id":23,
+        "state_dis":1.15,
+        "state_household_work_demanded": 76623.3,
+        "state_person_demanded_work": 126307.2
+        },
+        {  
+        "id":24,
+        "state_dis":1.04,
+        "state_household_work_demanded": 83086.18,
+        "state_person_demanded_work": 106758.59
+        },
+        {  
+        "id":25,
+        "state_dis":0.97,
+        "state_household_work_demanded": 40289.04,
+        "state_person_demanded_work": 54622.23
+        },
+        {  
+        "id":26,
+        "state_dis":2.13,
+        "state_household_work_demanded": 385897,
+        "state_person_demanded_work": 612286.38
+        }
+    ],
+    "2018": [
+        { 
+        "id":1,
+        "state_dis":0.99,
+        "state_household_work_demanded":110665.42,
+        "state_person_demanded_work": 121381.52
+        },
+        {  
+        "id":2,
+        "state_dis":1.73,
+        "state_household_work_demanded":334966.63,
+        "state_person_demanded_work": 576316.88
+        },
+        {  
+        "id":3,
+        "state_dis":0.6,
+        "state_household_work_demanded":3596.92,
+        "state_person_demanded_work": 3822.06
+        },
+        {  
+        "id":4,
+        "state_dis":1.31,
+        "state_household_work_demanded":32189.89,
+        "state_person_demanded_work": 46072.39
+        },
+        {  
+        "id":5,
+        "state_dis":0.95,
+        "state_household_work_demanded":64022.81,
+        "state_person_demanded_work": 76566.33
+        },
+        {  
+        "id":6,
+        "state_dis":1.2,
+        "state_household_work_demanded":81371.94,
+        "state_person_demanded_work": 156119.42
+        },
+        {  
+        "id":7,
+        "state_dis":0.01,
+        "state_household_work_demanded":229.6,
+        "state_person_demanded_work": 234.2
+        },
+        {  
+        "id":8,
+        "state_dis":0.78,
+        "state_household_work_demanded":24499.31,
+        "state_person_demanded_work": 40433.84
+        },
+        {  
+        "id":9,
+        "state_dis":0.52,
+        "state_household_work_demanded":8873.07,
+        "state_person_demanded_work": 12933.14
+        },
+        {  
+        "id":10,
+        "state_dis":1.42,
+        "state_household_work_demanded":43740.72,
+        "state_person_demanded_work": 55909.98
+        },
+        {  
+        "id":11,
+        "state_dis":0.91,
+        "state_household_work_demanded":8538.75,
+        "state_person_demanded_work": 11505.59
+        },
+        {  
+        "id":12,
+        "state_dis":0.92,
+        "state_household_work_demanded":52109.97,
+        "state_person_demanded_work": 68746.59
+        },
+        {  
+        "id":13,
+        "state_dis":0.86,
+        "state_household_work_demanded":34155.73,
+        "state_person_demanded_work": 66534.84
+        },
+        {  
+        "id":14,
+        "state_dis":1.22,
+        "state_household_work_demanded":84092.83,
+        "state_person_demanded_work": 95473.05
+        },
+        {  
+        "id":15,
+        "state_dis":0.98,
+        "state_household_work_demanded":58704.1,
+        "state_person_demanded_work": 100912.97
+        },
+        {  
+        "id":16,
+        "state_dis":1.04,
+        "state_household_work_demanded":39469.05,
+        "state_person_demanded_work": 73174.69
+        },
+        {  
+        "id":17,
+        "state_dis":1.1,
+        "state_household_work_demanded":38527.79,
+        "state_person_demanded_work": 39433.75
+        },
+        {  
+        "id":18,
+        "state_dis":0.79,
+        "state_household_work_demanded":21916.39,
+        "state_person_demanded_work": 30100.78
+        },
+        {  
+        "id":19,
+        "state_dis":1.2,
+        "state_household_work_demanded":23157.56,
+        "state_person_demanded_work": 23485.25
+        },
+        {  
+        "id":20,
+        "state_dis":0.81,
+        "state_household_work_demanded":65049.48,
+        "state_person_demanded_work": 103383.3
+        },
+        {  
+        "id":21,
+        "state_dis":1.02,
+        "state_household_work_demanded":21751,
+        "state_person_demanded_work": 25123.2
+        },
+        {  
+        "id":22,
+        "state_dis":1.42,
+        "state_household_work_demanded":120649.61,
+        "state_person_demanded_work": 180560.48
+        },
+        {  
+        "id":23,
+        "state_dis":1.11,
+        "state_household_work_demanded":63277.6,
+        "state_person_demanded_work": 74588.4
+        },
+        {  
+        "id":24,
+        "state_dis":1.03,
+        "state_household_work_demanded":57300.46,
+        "state_person_demanded_work": 70240.57
+        },
+        {  
+        "id":25,
+        "state_dis":0.97,
+        "state_household_work_demanded":23146.04,
+        "state_person_demanded_work": 30661.65
+        },
+        {  
+        "id":26,
+        "state_dis":2.45,
+        "state_household_work_demanded":169149.69,
+        "state_person_demanded_work": 264534.16
+        }
+    ]
+}
